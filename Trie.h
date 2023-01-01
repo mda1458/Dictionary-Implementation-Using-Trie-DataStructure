c07558d66adc3984a8e363abb2c2cd35a72d899e
Muhammad Danish Azeem <Muhammaddanish1458@gmail.com>
2023-01-02 03:40:55 +0500

    Added a function for optimization
    Optimized the code from 256 character to 73 Characters

--- Trie.h
+++ Trie.h
@@ -9,14 +9,14 @@ class TrieNode
 {
 public:
     Dictionary *dict;        // Dictionary object made from Dictionary.h
-    TrieNode *children[256]; // 256 children for 256 ASCII characters
+    TrieNode *children[73]; // 73 children for 73 ASCII characters
     bool isEndOfWord;        // true if the node represents end of a word
 
     TrieNode() // Constructor
     {
         dict = NULL;
         isEndOfWord = false;
-        for (int i = 0; i < 256; i++)
+        for (int i = 0; i < 73; i++)
             children[i] = NULL;
     }
 };
@@ -31,13 +31,45 @@ public:
         root = new TrieNode();
     }
 
+    int returnCharIdx(char c) //Return the index of char
+    {
+        // ".", "-"
+        if (c == 46 || c == 45)
+        {
+            c = c - 45; // Maps to index 0 and 1
+        }
+        // num 
+        else if (c >= 48 && c <= 57)
+        {
+            c = c - 45; // Maps to index 2 to 12
+        }
+        // A-Z
+        else if (c >= 65 && c <= 90)
+        {
+            c = c - 52; // Maps to index 13 to 38
+        }
+        // a-z
+        else if (c >= 97 && c <= 122)
+        {
+            c = c - 58; // Maps to index 39 to 64
+        }
+        // " ", "!", """, "#", "$", "%", "&", "'"
+        else if (c >= 32 && c <= 39)
+        {
+            c = c + 33; // Maps to index 65 to 73
+        }
+        return c;
+    }
+
     void insert(Dictionary dict) // Insert a word into the trie
     {
+        
         TrieNode *temp = root;
         // loop through all the characters of the word
         for (int i = 0; i < dict.word.length(); i++)
         {
-            int index = dict.word[i];
+            char c = dict.word[i];
+            int index = returnCharIdx(c);
             if (temp->children[index] == NULL) // if the character is not present in the trie
                 temp->children[index] = new TrieNode();
             temp = temp->children[index];
@@ -51,7 +83,8 @@ public:
         TrieNode *temp = root;                  // start from the root node
         for (int i = 0; i < word.length(); i++) // loop through all the characters of the word
         {
-            int index = word[i];
+            char c = word[i];
+            int index = returnCharIdx(c);
             if (temp->children[index] == NULL) // if the character is not present in the trie
                 return Dictionary("", "", ""); // return an empty dictionary object
             temp = temp->children[index];
@@ -66,7 +99,7 @@ public:
     {
         if (temp->isEndOfWord) // Base case -> will print the word
             cout << *temp->dict << endl;
-        for (int i = 0; i < 256; i++)
+        for (int i = 0; i < 73; i++)
         {
             if (temp->children[i] != NULL)
             {
@@ -82,7 +115,8 @@ public:
         TrieNode *temp = root;
         for (int i = 0; i < word.length(); i++) // Traverse upto last character of the word
         {
-            int index = word[i];
+            char c = word[i];
+            int index = returnCharIdx(c);
             if (temp->children[index] == NULL)
                 return;
             temp = temp->children[index];
@@ -93,7 +127,7 @@ public:
 
     void destroy(TrieNode *temp) // Destroy the trie
     {
-        for (int i = 0; i < 256; i++)
+        for (int i = 0; i < 73; i++)
         {
             if (temp->children[i] != NULL)
                 destroy(temp->children[i]);
@@ -113,22 +147,23 @@ void writeToFileUtil(TrieNode *root, fstream &myfile)
         myfile << root->dict->word << "," << root->dict->pos << "," << root->dict->meaning << endl;
     }
 
-    for (int i = 0; i < 256; i++)
+    for (int i = 0; i < 73; i++)
         writeToFileUtil(root->children[i], myfile);
 }
 
 // Filter the words by prefix and return a trie with these words
-Trie filterWords(Trie trie, string prefix)
+Trie *filterWords(Trie *trie, string prefix)
 {
-    TrieNode *temp = trie.root;
+    TrieNode *temp = trie->root;
     for (int i = 0; i < prefix.length(); i++) // loop through all the characters of the prefix
     {
-        int index = prefix[i];
+        char c = prefix[i];
+        int index = trie->returnCharIdx(c);
         if (temp->children[index] == NULL) // if the character is not present in the trie
-            return Trie();                 // return an empty trie
+            return new Trie();                 // return an empty trie
         temp = temp->children[index];      // move to the next node
     }
-    Trie filteredTrie;
-    filteredTrie.root = temp; // set the root of the filtered trie to the last node of the prefix
+    Trie *filteredTrie = new Trie();
+    filteredTrie->root = temp; // set the root of the filtered trie to the last node of the prefix
     return filteredTrie;      // return the filtered trie
 }
